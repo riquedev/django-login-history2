66d77184ef672464fcd927ddbf5539a6137d8175
riquedev <rique_dev@hotmail.com>
2023-08-26 00:13:55 -0300

    :sparkles: added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_login_history",
+    version="0.0.1",
+    author="Henrique da Silva Santos",
+    author_email="rique_dev@hotmail.com",
+    description="It's easy to use, plug-in django app that once included, stores logins history (with device data, like IP, user-agent, location etc.) of all users",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT License",
+    url="https://github.com/riquedev/django-login-history2",
+    keywords="security, django, login, location, session, ip, authenticate",
+    project_urls={
+        "Bug Tracker": "https://github.com/riquedev/django-login-history2/issues",
+        "Repository": "https://github.com/riquedev/django-login-history2",
+    },
+    install_requires=[
+        'Django>=3.2',
+    ],
+    classifiers=[
+        "App :: Django",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8"
+)
