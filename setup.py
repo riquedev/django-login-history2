71c7804655c5ee799f86c3b9691b1a40200c64db
Henrique da Silva Santos <rique_dev@hotmail.com>
2025-02-27 12:49:26 -0300

    :bookmark: Updated version management to use external VERSION file
    Moved the version definition to an external `VERSION` file for better maintainability and separation of concerns. The `setup.py` script now reads the version dynamically, reducing the need for manual updates in multiple places.

--- setup.py
+++ setup.py
@@ -1,47 +1,50 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh_readme:
 
-setuptools.setup(
-    name="django_login_history2",
-    version="0.0.7",
-    author="Henrique da Silva Santos",
-    author_email="rique_dev@hotmail.com",
-    description="It's easy to use, plug-in django app that once included, stores logins history (with device data, like IP, user-agent, location etc.) of all users",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT License",
-    url="https://github.com/riquedev/django-login-history2",
-    keywords="security, django, login, location, session, ip, authenticate",
-    project_urls={
-        "Bug Tracker": "https://github.com/riquedev/django-login-history2/issues",
-        "Repository": "https://github.com/riquedev/django-login-history2",
-    },
-    install_requires=[
-        'Django>=3.2.13',
-        'django_ipware>=4.0.2',
-        'requests>=2.31.0',
-    ],
-    extras_require={
-        'dev': [
-            'pytest',
-            'tox',
-            'black'
-        ],
-    },
-    classifiers=[
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    packages=setuptools.find_packages(exclude=("tests", 'django_login_history', "manage.py")),
-    include_package_data=True,
-    python_requires=">=3.8"
-)
+    with open("VERSION", "r", encoding="utf-8") as fh_version:
+        version = fh_version.read()
+        long_description = fh_readme.read()
+
+        setuptools.setup(
+            name="django_login_history2",
+            version=version,
+            author="Henrique da Silva Santos",
+            author_email="rique_dev@hotmail.com",
+            description="It's easy to use, plug-in django app that once included, stores logins history (with device data, like IP, user-agent, location etc.) of all users",
+            long_description=long_description,
+            long_description_content_type="text/markdown",
+            license="MIT License",
+            url="https://github.com/riquedev/django-login-history2",
+            keywords="security, django, login, location, session, ip, authenticate",
+            project_urls={
+                "Bug Tracker": "https://github.com/riquedev/django-login-history2/issues",
+                "Repository": "https://github.com/riquedev/django-login-history2",
+            },
+            install_requires=[
+                'Django>=3.2.13',
+                'django_ipware>=4.0.2',
+                'requests>=2.31.0',
+            ],
+            extras_require={
+                'dev': [
+                    'pytest',
+                    'tox',
+                    'black'
+                ],
+            },
+            classifiers=[
+                "Framework :: Django",
+                "Intended Audience :: Developers",
+                "Programming Language :: Python :: 3",
+                "Programming Language :: Python :: 3.8",
+                "Programming Language :: Python :: 3.9",
+                "Programming Language :: Python :: 3.10",
+                "Programming Language :: Python :: 3.11",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+            ],
+            packages=setuptools.find_packages(exclude=("tests", 'django_login_history', "manage.py")),
+            include_package_data=True,
+            python_requires=">=3.8"
+        )
